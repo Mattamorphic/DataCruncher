36e048772df391e68f2b20065968e5e8dbdca082
Matt Barber <mattbarber@redweb.com>
2016-08-10 14:28:22 +0100

    Final casing changes

--- tests/integration/Segmentation/QueryTest.php
+++ tests/integration/Segmentation/QueryTest.php
@@ -4,7 +4,7 @@ use mfmbarber\Data_Cruncher\Segmentation\Query as Query;
 use mfmbarber\Data_Cruncher\Helpers\CSVFile as CSVFile;
 class QueryTest extends \PHPUnit_Framework_TestCase
 {
-    const SMALL_TEST = './Tests/integration/Segmentation/test_small.csv';
+    const SMALL_TEST = './tests/integration/Segmentation/test_small.csv';
 
     /**
      * @test
