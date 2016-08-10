d3df7ff15acd020461a6ab8e57201971966cdd0b
Matt Barber <mattbarber@redweb.com>
2016-08-10 19:49:53 +0100

    Improved test coverage

---
+++ tests/unit/Config/ValidationTest.php
@@ -0,0 +1,64 @@
+<?php 
+namespace mfmbarber\Data_Cruncher\Tests\Unit\Config;
+
+use mfmbarber\Data_Cruncher\Config\Validation as Validation;
+
+class ValidationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+    **/
+    public function isNormalArrayReturnsTrue()
+    {
+        $this->assertTrue(Validation::isNormalArray(['test']));
+    }
+
+    /**
+     * @test
+    **/
+    public function isNormalArrayReturnsFalse()
+    {
+        $this->assertFalse(Validation::isNormalArray(['a' => 'b']));
+    }
+    
+    /**
+     * @test
+    **/
+    public function isNormalArrayReturnsFalseGivenSize()
+    {
+        $this->assertFalse(Validation::isNormalArray(['a', 'b'], 3));
+    }
+
+    /**
+     * @test
+    **/
+    public function isAssociativeArrayReturnsTrue()
+    {
+        $this->assertTrue(Validation::isAssociativeArray(['a' => 'b']));
+    }
+
+    /**
+     * @test
+    **/
+    public function isAssociativeArrayReturnsFalse()
+    {
+        $this->assertFalse(Validation::isAssociativeArray(['a', 'b']));
+    }
+
+    /**
+     * @test
+    **/
+    public function getDateTimeReturnsDateObj()
+    {
+        $date = \DateTime::createFromFormat('d/m/Y', '24/11/1987');
+        $this->assertEquals($date, Validation::getDateTime('24/11/1987', 'd/m/Y'));
+    }
+
+    /**
+     * @test
+    **/
+    public function getDateTimeReturnsFalse()
+    {
+        $this->assertFalse(Validation::getDateTime('a', 'Y'));
+    }
+}
