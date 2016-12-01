5247444e007eb4f355b0818dc5263a547d073fe5
Matt Barber <MattBarber@redweb.com>
2016-12-01 12:14:55 +0000

    Initial Query Integration Tests File

---
+++ tests/integration/QueryTests/QueryTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace mfmbarber\DataCruncher\Tests\Integration\QueryTests;
+
+use mfmbarber\DataCruncher\Config\Validation;
+use mfmbarber\DataCruncher\Helpers\DataSource;
+use mfmbarber\DataCruncher\Segmentation\Query;
+
+
+class QueryTest extends \PHPUnit_Framework_TestCase {
+
+    public function testItShouldQueryACSVFile(){}
+    public function testItShouldQueryAXMLFile(){}
+    public function testItShouldQueryADatabaseTable(){}
+    public function testItShouldOutputToCode(){}
+    public function testItShouldOutputToCSV(){}
+    public function testItShouldOutputToXML(){}
+    public function testItShouldOutputToDBTable(){}
+    public function testItShouldQueryASingleField(){}
+    public function testItShouldQueryMultipleFields(){}
+    public function testItShouldQueryEquals(){}
+    public function testItShouldQueryComparison(){}
+    public function testItShouldQueryLike(){}
+    public function testItShouldQueryEmpty(){}
+    public function testItShouldQueryDates(){}
+    public function testItShouldQueryStrings(){}
+    public function testItShouldQueryInts(){}
+
+
+
+}
